4b283a225cbf3e7a66161508d515adb9948f33db
unknown <lh.pedrosot@hotmail.com>
2020-05-08 03:41:17 -0300

    release

---
+++ precache-manifest.c33e31e6890165052f51345447ef0005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba36d12f5f8db74c6e39b1033b7d049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1be5cbdbd9e3841795f",
+    "url": "/static/css/main.52b9d2e7.chunk.css"
+  },
+  {
+    "revision": "f5869b33b72dfedde5a5",
+    "url": "/static/js/2.6e4c8331.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6e4c8331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1be5cbdbd9e3841795f",
+    "url": "/static/js/main.f8fef9c2.chunk.js"
+  },
+  {
+    "revision": "a889637e45429599bc2e",
+    "url": "/static/js/runtime-main.f31d8e25.js"
+  }
+]);
